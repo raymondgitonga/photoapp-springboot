0067e8b5d5a1aaee3939d61818ef6a86800ce4bb
raymond gitonga <raytosh95@gmail.com>
2020-03-19 00:29:51 +0300

    Initial commit

---
+++ src/test/java/com/tosh/userspring/UserSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tosh.userspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
