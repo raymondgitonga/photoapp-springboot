f9b6a6dc810da6e1d5127b45fdb8bf1dacef83dd
raymond gitonga <raytosh95@gmail.com>
2020-03-23 23:14:51 +0300

    Add jwt

--- src/main/java/com/tosh/userspring/security/SecurityConstants.java
+++ src/main/java/com/tosh/userspring/security/SecurityConstants.java
@@ -5,5 +5,5 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer: ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_STRING = "jf9i4jgu83nf10";
+    public static final String TOKEN_SECRET = "jf9i4jgu83nf10";
 }
