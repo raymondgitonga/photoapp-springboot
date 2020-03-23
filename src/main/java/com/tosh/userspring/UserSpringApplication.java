d4b255aaa28ff27349bab838feefe4977374dc50
raymond gitonga <raytosh95@gmail.com>
2020-03-24 01:04:14 +0300

    move properties to properties to properties field

--- src/main/java/com/tosh/userspring/UserSpringApplication.java
+++ src/main/java/com/tosh/userspring/UserSpringApplication.java
@@ -1,5 +1,6 @@
 package com.tosh.userspring;
 
+import com.tosh.userspring.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -20,6 +21,11 @@ public class UserSpringApplication {
 	@Bean
 	public SpringApplicationContext springApplicationContext(){
 		return new SpringApplicationContext();
+	}
+
+	@Bean(name = "AppProperties")
+	public AppProperties getAppProperties(){
+		return new AppProperties();
 	}
 
 }
--- src/main/java/com/tosh/userspring/security/SecurityConstants.java
+++ src/main/java/com/tosh/userspring/security/SecurityConstants.java
@@ -1,9 +1,17 @@
 package com.tosh.userspring.security;
 
+import com.tosh.userspring.SpringApplicationContext;
+
 public class SecurityConstants {
     public static final long EXPERATION_DATE = 864000000;
     public static final String TOKEN_PREFIX = "Bearer: ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users";
-    public static final String TOKEN_SECRET = "jf9i4jgu83nf10";
+
+
+    public  static String getTokenSecret(){
+        AppProperties appProperties = (AppProperties) SpringApplicationContext.getBean("AppProperties");
+
+        return appProperties.getTokenSecret();
+    }
 }
