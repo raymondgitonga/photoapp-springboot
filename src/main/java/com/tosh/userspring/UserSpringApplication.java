6c791383c56d0cc277fc9caa22ad06f41bbe2f7e
raymond gitonga <raytosh95@gmail.com>
2020-03-25 04:21:54 +0300

    Convert file to war

--- src/main/java/com/tosh/userspring/UserSpringApplication.java
+++ src/main/java/com/tosh/userspring/UserSpringApplication.java
@@ -3,11 +3,18 @@ package com.tosh.userspring;
 import com.tosh.userspring.security.AppProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class UserSpringApplication {
+public class UserSpringApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(UserSpringApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UserSpringApplication.class, args);
