c4a0c5ec4ca83cd13d39a002f21f2ccf9edc896f
raymond gitonga <raytosh95@gmail.com>
2020-03-25 03:40:31 +0300

    Add context path

--- src/main/java/com/tosh/userspring/security/AuthorizationFilter.java
+++ src/main/java/com/tosh/userspring/security/AuthorizationFilter.java
@@ -5,14 +5,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import sun.plugin.liveconnect.SecurityContextHelper;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.security.Security;
 import java.util.ArrayList;
 
 public class AuthorizationFilter extends BasicAuthenticationFilter {
